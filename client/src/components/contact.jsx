f66d4803896cfee26e64c460ec583edf9ebccf17
DrBig-Brain <abhinav714b@gmail.com>
2026-05-29 00:41:26 +0530

    pre-final done

--- client/src/components/contact.jsx
+++ client/src/components/contact.jsx
@@ -2,50 +2,29 @@ import "../styles.css";
 
 export const Contact = () => {
     return(
-        <div className="pt-2 pb-2 bg-background-secondary border-t-2 border-action-primary">
-            <div className="flex flex-col items-center">
-                <div className="text-action-primary">
-                    <h3 className="text-5xl text-center font-bold">Abhinav Mishra</h3>
-                    <p className="text-3xl text-center">Developer/Learner/Explorer/Thinker/Builder</p>
+        <div className="pt-2 pb-2 bg-background-secondary border-t-2 border-action-primary" id="contact">
+            <div className="flex flex-row px-4 py-4">
+                <div className="flex flex-col w-1/3 items-centered">
+                    <div className="pb-2"><h1 className="text-white-main text-4xl">Abhinav Mishra</h1></div>
+                    <div className="py-2"><p className="text-action-secondary text-2xl">Developer/ Learner/ Explorer/ Builder</p></div>
+                    <div className="py-2"><div className="rounded-2xl border-2 border-white-main p-4 w-fit"><a className="flex items-center gap-3" href="mailto:abhinav714b@gmail.com"><img className="h-6 w-6 invert" src="https://www.svgrepo.com/show/305463/email-outline.svg" alt="email icon"/><p className="text-white-main hover:underline">abhinav714b@gmail.com</p></a></div></div>
                 </div>
-                <p className="text-5xl text-action-primary font-bold pt-4">Socials</p>
-                <div className="pt-6 flex flex-row text-white-main">
-                    <a className="px-20 flex flex-col items-center hover:-translate-y-2 transition-all duration-300" href = "https://github.com/DrBig-Brain" target="_blank">
-                        <img className="pb-4 h-[50px] w-[50px] invert" src="https://simpleicons.org/icons/github.svg"></img>
-                        <p>Github</p>
-                    </a>
-                    <a className="px-20 flex flex-col items-center hover:-translate-y-2 transition-all duration-300" href = "https://www.linkedin.com/in/abhinavmishra05/" target="_blank">
-                        <img className="pb-4 h-[50px] w-[50px] invert" src="https://cdn-icons-png.flaticon.com/128/49/49656.png"></img>
-                        <p>Linkedin</p>
-                    </a>
-                    <a className="px-20 flex flex-col items-center hover:-translate-y-2 transition-all duration-300" href = "https://leetcode.com/u/DrBig-Brain/" target="_blank">
-                        <img className="pb-4 px-2 h-[50px] w-[50px] invert" src="https://simpleicons.org/icons/leetcode.svg"></img>
-                        <p>Leetcode</p>
-                    </a>
-                    <a className="px-20 flex flex-col items-center hover:-translate-y-2 transition-all duration-300" href = "https://www.kaggle.com/abhinav10101010"  target="_blank">
-                        <img className="pb h-[50px] w-[50px] invert" src="https://simpleicons.org/icons/kaggle.svg"></img>
-                        <p>Kaggle</p>
-                    </a>
-                    <a className="px-20 flex flex-col items-center hover:-translate-y-2 transition-all duration-300" href = "https://huggingface.co/DrBig-Brain" target="_blank">
-                        <img className="pb-4 h-[50px] w-[50px] invert" src="https://simpleicons.org/icons/huggingface.svg"></img>
-                        <p>HuggingFace</p>
-                    </a>
-                </div>
-                <p className="text-5xl pt-4 text-action-primary font-bold">Or Reach Out</p>
-                <div className="pt-4 flex flex-row text-white-main text-3xl">
-                    <a className="flex flex-row items-center gap-2 hover:-translate-y-2 transition-all duration-300" href="mailto:abhinav714b@gamil.com">
-                        <img className="h-[32px] w-[32px] invert pr-2" src="https://simpleicons.org/icons/gmail.svg"></img>
-                        <p className="text-2xl">Mail me</p>
-                    </a>
-                    <p className="px-6 text-action-primary">OR</p>
-                    <div className="flex flex-row items-center gap-2 hover:-translate-y-2 transition-all duration-300">
-                        <img className="h-[32px] w-[32px] invert pr-2" src="https://cdn-icons-png.flaticon.com/128/597/597177.png"></img>
-                        <p className="text-2xl">+91-7807952061</p>
+                <div className="w-2/3 px-2">
+                    <h1 className="text-action-secondary text-3xl">// Find me on</h1>
+                    <div className="flex flex-row">
+                        <div className="pr-4"><div className="p-4 h-full w-full bg-background rounded-2xl border-2 border-action-primary"><a className="flex flex-col items-center" target="_blank" href="https://github.com/DrBig-Brain"><img className="p-2 invert h-1/2 w-1/2" src="https://simpleicons.org/icons/github.svg"></img><p className="text-action-primary text-xl">Github</p></a></div></div>
+                        <div className="pl-4"><div className="p-4 h-full w-full bg-background rounded-2xl border-2 border-action-primary"><a className="flex flex-col items-center" target="_blank" href="https://www.linkedin.com/in/abhinavmishra05/"><img className="p-2 invert h-1/2 w-1/2" src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/linkedin-square-icon.svg"></img><p className="text-action-primary text-xl">Linkedin</p></a></div></div>
+                        <div className="px-4"><div className="p-4 h-full w-full bg-background rounded-2xl border-2 border-action-primary"><a className="flex flex-col items-center" target="_blank" href="https://huggingface.co/DrBig-Brain"><img className="p-2 invert h-1/2 w-1/2" src="https://simpleicons.org/icons/huggingface.svg"></img><p className="text-action-primary text-xl">Huggingface</p></a></div></div>
+                        <div className="px-4"><div className="p-4 h-full w-full bg-background rounded-2xl border-2 border-action-primary"><a className="flex flex-col items-center" target="_blank" href="https://www.kaggle.com/abhinav10101010"><img className="p-2 invert h-1/2 w-1/2" src="https://simpleicons.org/icons/kaggle.svg"></img><p className="text-action-primary text-xl">Kaggle</p></a></div></div>
+                        <div className="px-4"><div className="p-4 h-full w-full bg-background rounded-2xl border-2 border-action-primary"><a className="flex flex-col items-center" target="_blank" href="https://leetcode.com/problemset/"><img className="p-2 invert h-1/2 w-1/2" src="https://simpleicons.org/icons/leetcode.svg"></img><p className="text-action-primary text-xl">Leetcode</p></a></div></div>
                     </div>
                 </div>
-
             </div>
-
-        </div>
+            <div className="px-20 py-4"><hr className=" px-10 border-action-secondary"></hr></div>
+            <div className="flex justify-between items-baseline py-4 px-4">
+                <div className="px-2"><p className="text-l text-action-secondary">© 2026 • <span className="text-action-primary">Abhinav Mishra</span> • Built with React • NIT Hamirpur <a href="https://github.com/DrBig-Brain/myPortfolio">scouce code</a> </p></div>
+                <div className="rounded-2xl border-2 border-white-main"><a href="#about" className="p-2 text-2xl text-white-main">Back to the top!</a></div>
+            </div>
+        </div> 
     )
 }
